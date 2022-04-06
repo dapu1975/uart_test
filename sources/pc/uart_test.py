ae98c133b31a3b6dbbb4999de796ee1eb3beb5e4
Daniel Pust <pust.daniel@outlook.com>
2022-04-06 18:40:46 +0200

    update

--- sources/pc/uart_test.py
+++ sources/pc/uart_test.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+import serial
+import uuid
+
 print("***Serial Test Tool *** ")
 
-# setup tty with pyserial
+try:
+    ser = serial.Serial(
+        port='/dev/ttyACM0',
+        baudrate=9600,
+        rtscts=0
+    )
+    ser.close()
+    ser.open()
+    if ser.isOpen():
+        myuuid = uuid.uuid4()
+        print('Your UUID is: ' + str(myuuid))
+        ser.write(str.encode(str(myuuid)))
+        dummy = ser.readline()
+        print(dummy)
+    else:
+        print("port not ready")
+except Exception as e:
+    print(e)
+finally:
+    ser.close()
+    print("***end***")
